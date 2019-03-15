27272ec16aff2fbf490b360b505e51775d7fe2d4
Felix1663022 <47707918+Felix1663022@users.noreply.github.com>
2019-03-15 00:04:27 -0600

    Update ecuaciones.m

--- ecuaciones.m
+++ ecuaciones.m
@@ -1,3 +1,4 @@
+%% ecuaciones.m
 function dxdt=ecuaciones(t,x)
 a=0.4;
 b=0.4;
@@ -5,3 +6,14 @@ g=2;
 s=0.09;
 dxdt=[(a*x(1))-(b*x(1)*x(2));(s*x(1)*x(2))-(g*x(2))];
 end
+ %% call_ecuaciones.m
+function[t,x]=call_ecuacioness()
+tspan=[0 100];
+x0=[0;-1;-1]
+[t,x]=ode45(@ecuacioness,tspan,x0);
+end
+%%
+[t,x]=call_ecuaciones();
+plot(t,x(:,1))
+figure
+plot(t,x(:,2))
